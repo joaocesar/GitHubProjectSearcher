286aee6bbdb0528b2f2d39a41db4fe2488e11415
Joao Cesar Pereira <joao.cesar@gmail.com>
2017-09-23 15:04:20 -0300

    Initial Commit

---
+++ src/main/java/br/eti/jcp/projects/githubprojectsearcher/GitHubProjectSeacherApplication.java
@@ -0,0 +1,12 @@
+package br.eti.jcp.projects.githubprojectsearcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubProjectSeacherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubProjectSeacherApplication.class, args);
+	}
+}
---
+++ src/test/java/br/eti/jcp/projects/githubprojectsearcher/GitHubProjectSeacherApplicationTests.java
@@ -0,0 +1,16 @@
+package br.eti.jcp.projects.githubprojectsearcher;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GitHubProjectSeacherApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
